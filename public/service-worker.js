8f3e3088490ec37a21fa26a13d68e2d5f8485908
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-10 15:29:35 -0800

    added install function to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,34 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  '/',
+  '/db.js',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/css/styles.css',
+  '/icon/icon-192x192.png',
+  '/icon/icon-512x512.png',
+];
+
+
 // install event handler
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('static').then( cache => {
-      return cache.addAll([
-        '/',
-        '/db.js',
-        '/index.html',
-        '/manifest.webmanifest',
-        '/css/styles.css',
-        '/icon/icon-192x192.png',
-        '/icon/icon-512x512.png',
-      ]);
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      console.log("Pre-cached files stored successfully");
+      return cache.addAll(FILES_TO_CACHE)
     })
   );
-  console.log('Install');
+  console.log('Installed');
   self.skipWaiting();
 });
 
 // retrieve assets from cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then( response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then( response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
